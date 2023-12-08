eb3fdac9d68136f3cb68165cf45e265f8eeb4ab6
archit sharma <architsharma29@gmail.com>
2023-12-08 18:51:14 +0530

    sesssion clearing added

--- helpers/errorValidator/errorValidator.ts
+++ helpers/errorValidator/errorValidator.ts
@@ -4,10 +4,10 @@ export const validator = async (req, res, redirect, ajax = false) => {
     try {
         if (req.joiError) {
             if (ajax) {
-                await res.send({ err: req.joiError });
+                res.send({ err: req.joiError });
             } else {
                 req.flash('error', req.joiError);
-                await res.redirect(redirect);
+                res.redirect(redirect);
             }
             return false; // Validation failed
         }
